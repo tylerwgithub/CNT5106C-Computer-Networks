ac9b95107180972ad429ae6aa5d25f426c9e962c
tylerwgithub <wangtongyu@ufl.edu>
2018-11-25 17:39:07 -0500

    Add files via upload

--- Download.java
+++ Download.java
@@ -1,94 +1,93 @@
 import java.io.IOException;
-import java.net.Socket;
-import java.util.concurrent.Callable;
+import java.net.*;
+import java.util.concurrent.*;
 
-public class Download implements Callable<Object>
-{
-  private Record record;
-  private BitField bitfield;
-  private Socket socket;
-  private Filemanger filemnger;
-  private Record[] neighbors;
-  private MyLogger logger;
-  private int peerID1;
-  
-public Download(int peerID1, Record record, Record[] neighbors, BitField bitfield, Filemanger filemnger, MyLogger logger) {
+public class Download implements Callable<Object> {
+	
+	private Record record;
+	private BitField myBitField;
+	private Socket socket;
+	private FileManager fileManager;
+	private Record[] neighbors;
+	private MyLogger logger;
+	private int myID;
+	
+	public Download(int myID, Record record, Record[] neighbors, BitField myBitField, FileManager fileManager, MyLogger logger) {
 		
-	this.peerID1 = peerID1;
-	this.neighbors = neighbors;
-	this.record = record;
-	this.bitfield = bitfield;
-	this.socket = record.getDownloadSocket();
-	this.filemnger = filemnger;
-	this.logger = logger;
+		this.myID = myID;
+		this.neighbors = neighbors;
+		this.record = record;
+		this.myBitField = myBitField;
+		this.socket = record.getDownloadSocket();
+		this.fileManager = fileManager;
+		this.logger = logger;
 		
 	}
-  
-  public Object call()
-    throws IOException
-  {
-    Message localMessage = new Message();
-    
-    int i;
-    do
-    {
-      localMessage.read(socket);
-      //when receiving STOP message, break
-      if (localMessage.getType() == 8) {
-        for (i = 0; i < neighbors.length; i++) {
-          localMessage.send(neighbors[i].getHaveSocket());
-        }
-        break;
-      }
-      
-    } while (localMessage.getType() != 1);
-    //when receiving CHOKE message or not interests to the sender
-    logger.UnchokingLog(record.getID());
-    for (;;) {
-      i = bitfield.getInterestingIndex(record.getBitField());
-      // send out not interested message
-      if (i == -1) {
-        localMessage.setType(3);
-        localMessage.setPayLoad(null);
-        localMessage.send(socket);
-        break;
-      }
-      //send out request message
-      localMessage.setType(6);
-      localMessage.setPayLoad(Conversion.IntToBytes(i));
-      localMessage.send(socket);
-      
-
-      localMessage.read(socket);
-      
-
-      if (localMessage.getType() == 0) {
-        logger.ChokingLog(record.getID());
-        break;
-      }
-      
-      Piece localPiece = new Piece(i, localMessage.getPayLoad());
-      filemnger.writepiece(localPiece);
-      bitfield.turnonbit(i);
-      
-      record.incDownload();
-      logger.DownloadLog(record.getID(), i);
-      if (bitfield.finished()) {
-        logger.CompletionLog();
-      }
-      
-	//send out have message to all the neighbors
-      localMessage.setType(4);
-      localMessage.setPayLoad(Conversion.IntToBytes(i));
-      for (int j = 0; j < neighbors.length; j++) {
-        localMessage.send(neighbors[j].getHaveSocket());
-      }
-    }
-    
-
-
-
-
-    return new Object();
-  }
+	
+	public Object call() throws IOException {
+		
+		Message msg = new Message();
+//		System.out.println("Download:30: peer " + this.myID + " prepares to download from peer " + record.getID());
+		//jump out the while loop only when receiving STOP message
+		while (true) {
+//			System.out.println("Download:33: peer " + this.myID + " before receives a message from peer " + record.getID());
+			msg.read(socket);
+//			System.out.println("Download:35: peer " + this.myID + " receives a message + from peer " + record.getID());
+			if (msg.getType() == Message.STOP) {
+				for (int i = 0; i < neighbors.length; i++) {
+					msg.send(neighbors[i].getHaveSocket());
+				}
+				break;
+			}
+			
+			else if (msg.getType() == Message.UNCHOKE) {
+				//jump out the while loop only when receiving CHOKE message or not interests to the sender
+				logger.unchokingLog(record.getID());
+				while (true) {
+					int interestedPiece = myBitField.getInterestingIndex(record.getBitField());
+//					System.out.println("Download:48:get a interested Piece:" + interestedPiece);
+					if (interestedPiece == -1) {
+						msg.setType(Message.NOTINTERESTED);
+						msg.setPayLoad(null);
+						msg.send(socket);
+						break;
+					} else {
+						//send REQUEST message
+						msg.setType(Message.REQUEST);
+						msg.setPayLoad(Conversion.IntToBytes(interestedPiece));
+						msg.send(socket);
+//						System.out.println("Download:59: sends REQUEST message");
+						//download a piece
+						msg.read(socket);
+//						System.out.println("Download:62:receives a message: type =" + msg.getType());
+						
+						if (msg.getType() == Message.CHOKE) {
+							logger.choking(record.getID());
+							break;
+						}
+						
+						Piece downloadPiece = new Piece(interestedPiece, msg.getPayLoad());
+						fileManager.writePiece(downloadPiece);
+						myBitField.turnOnBit(interestedPiece);
+//						System.out.println("Download:64:download a piece: bitField = " + myBitField.getText());
+						record.incDownload();
+						logger.downloadLog(record.getID(), interestedPiece);
+						if (myBitField.isFinished()) {
+							logger.compDownloadLog();
+						}
+								
+						//send HAVE message
+						msg.setType(Message.HAVE);
+						msg.setPayLoad(Conversion.IntToBytes(interestedPiece));
+						for (int i = 0; i < neighbors.length; i++) {
+							msg.send(neighbors[i].getHaveSocket());
+						}
+					}
+				}
+			} 
+			
+		}
+//		System.out.println("Downlaod:90:downlaod quit: peer: " + this.myID);
+		return new Object();
+	}
 }
